651e7ce9e778d9c655f28261d99218b118422221
PradeepDagar321 <pradeepdagar321@gmail.com>
2025-03-08 15:42:45 +0530

    modified:   App.js 	modified:   index.html
    	index.css

--- App.js
+++ App.js
@@ -9,3 +9,20 @@ root.render(x);
 const jsx = <h1 style={{ color: "red" }} >Hello from jsxx  </h1>
 const rt = ReactDOM.createRoot(document.getElementById("def"));
 rt.render(jsx);
+
+
+const Comp1 = () => { 
+    return (
+        <div id="q">
+            <Comp2/>
+        <h1>hello comp1</h1>
+    </div>
+    )
+};
+
+const Comp2 = () => {
+   return  <h1>hello comp2</h1>
+}
+
+const rt2 = ReactDOM.createRoot(document.getElementById("react_component"));
+rt2.render(<Comp1 />);
